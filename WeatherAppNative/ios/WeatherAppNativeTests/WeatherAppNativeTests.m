48ba92cc6e0f6689d371bfa8171a95d2ba936b13
Mark - DopestGeek <markmayalla@gmail.com>
2020-03-28 12:57:37 +0300

    Setup for React Native App

--- WeatherAppNative/ios/WeatherAppNativeTests/WeatherAppNativeTests.m
+++ WeatherAppNative/ios/WeatherAppNativeTests/WeatherAppNativeTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface WeatherAppNativeTests : XCTestCase
+@interface weatherappnativeTests : XCTestCase
 
 @end
 
-@implementation WeatherAppNativeTests
+@implementation weatherappnativeTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
